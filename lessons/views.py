05f369dd9812d3a2b674dc322657617926114f1c
goleiroAI <toinblue11@gmail.com>
2025-06-20 14:04:33 +0900

    Update views.py

--- lessons/views.py
+++ lessons/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Lesson
+from collections import defaultdict
+from django.http import HttpResponseForbidden
+
+ACCESS_KEY = "abc123"  # このキーだけ許可
 
 def lesson_list(request):
-    lessons = Lesson.objects.all().order_by('chapter', 'number')
-    return render(request, 'lessons/lesson_list.html', {'lessons': lessons})
+    key = request.GET.get("access", "")
+    if key != ACCESS_KEY:
+        return HttpResponseForbidden("このページは限定公開です。")
 
-def lesson_detail(request, pk):
-    lesson = get_object_or_404(Lesson, pk=pk)
-    return render(request, 'lessons/lesson_detail.html', {'lesson': lesson})
+    lessons = Lesson.objects.all().order_by('chapter', 'number')
+    grouped_lessons = defaultdict(list)
+    for lesson in lessons:
+        grouped_lessons[lesson.chapter].append(lesson)
+    return render(request, 'lessons/lesson_list.html', {'grouped_lessons': grouped_lessons, 'lessons': lessons})
