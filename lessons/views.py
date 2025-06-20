575009964732f98dc8f4749753eae26540650f45
goleiroAI <toinblue11@gmail.com>
2025-06-20 14:09:44 +0900

    Update views.py

--- lessons/views.py
+++ lessons/views.py
@@ -15,3 +15,8 @@ def lesson_list(request):
     for lesson in lessons:
         grouped_lessons[lesson.chapter].append(lesson)
     return render(request, 'lessons/lesson_list.html', {'grouped_lessons': grouped_lessons, 'lessons': lessons})
+from django.shortcuts import get_object_or_404
+
+def lesson_detail(request, pk):
+    lesson = get_object_or_404(Lesson, pk=pk)
+    return render(request, 'lessons/lesson_detail.html', {'lesson': lesson})
