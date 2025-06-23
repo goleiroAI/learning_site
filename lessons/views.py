2f3497b810e9c2d569e6702da226b51a2b033dfc
goleiroAI <toinblue11@gmail.com>
2025-06-23 10:40:29 +0900

    Update views.py

--- lessons/views.py
+++ lessons/views.py
@@ -14,9 +14,21 @@ def lesson_list(request):
     grouped_lessons = defaultdict(list)
     for lesson in lessons:
         grouped_lessons[lesson.chapter].append(lesson)
-    return render(request, 'lessons/lesson_list.html', {'grouped_lessons': grouped_lessons, 'lessons': lessons})
-from django.shortcuts import get_object_or_404
+
+    return render(request, 'lessons/lesson_list.html', {
+        'grouped_lessons': grouped_lessons,
+        'lessons': lessons,
+        'access_key': key
+    })
 
 def lesson_detail(request, pk):
-    lesson = get_object_or_404(Lesson, pk=pk)
-    return render(request, 'lessons/lesson_detail.html', {'lesson': lesson})
+    key = request.GET.get("access", "")
+    if key != ACCESS_KEY:
+        return HttpResponseForbidden("このページは限定公開です。")
+
+    lesson = Lesson.objects.get(pk=pk)
+   return render(request, 'lessons/lesson_detail.html', {
+    'lesson': lesson,
+    'access_key': request.GET.get('access', ''),
+})
+
