9cf50a039ee7d353a743b6abc80158106f3896cd
goleiroAI <toinblue11@gmail.com>
2025-06-23 15:57:17 +0900

    Update models.py

--- lessons/models.py
+++ lessons/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Lesson(models.Model):
-    chapter = models.CharField(max_length=100)  # 第1章 など
-    number = models.CharField(max_length=10)    # 1-1 など
-    title = models.CharField(max_length=200)    # レッスンタイトル
-    content = models.TextField(blank=True)      # レッスン本文（今は空欄OK）
+    chapter = models.CharField(max_length=20)
+    number = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.chapter} {self.number}: {self.title}"
+        return f"{self.chapter} {self.number} {self.title}"
